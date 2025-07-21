f031f68aee7168723225b368181c77c177b6b54d
Franco Sbaffi <francosbaffidev@gmail.com>
2025-07-21 04:25:57 -0300

    Make all Callout components use the same icon while keeping different colors

--- components/callout/Callout.tsx
+++ components/callout/Callout.tsx
@@ -1,11 +1,6 @@
 import { FC } from "react";
 import { Box, Icon, useStyleConfig } from "@chakra-ui/react";
-import {
-  RiInformationFill,
-  RiCheckboxCircleFill,
-  RiAlertFill,
-  RiCloseCircleFill,
-} from "react-icons/ri";
+import { RiInformationFill } from "react-icons/ri";
 import { WithChildren } from "@/types";
 
 interface CalloutProps extends WithChildren {
@@ -15,25 +10,8 @@ interface CalloutProps extends WithChildren {
 
 const Callout: FC<CalloutProps> = ({ size, variant, children }) => {
   const styles = useStyleConfig("Callout", { size, variant });
-  let icon;
-
-  switch (variant) {
-    case "info":
-      icon = RiInformationFill;
-      break;
-    case "success":
-      icon = RiCheckboxCircleFill;
-      break;
-    case "warning":
-      icon = RiAlertFill;
-      break;
-    case "error":
-      icon = RiCloseCircleFill;
-      break;
-    default:
-      icon = RiInformationFill;
-      break;
-  }
+  // Usar siempre el mismo icono para todos los callouts
+  const icon = RiInformationFill;
 
   return (
     <Box __css={styles}>
